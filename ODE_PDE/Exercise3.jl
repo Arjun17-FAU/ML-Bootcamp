96262793b61d1e1065e447ee7b4d58f1b53ce8f5
Arjun17 - FAU <arjunarjunsankar1998@gmail.com>
2024-09-18 00:54:28 +0100

    Ex3

--- ODE_PDE/Exercise3.jl
+++ ODE_PDE/Exercise3.jl
@@ -44,18 +44,35 @@ solution_i = sol[u_i(t,x)]
 # Plot the real part of the solution
 plt = plot()
 for i in eachindex(discrete_t)
-    plot!(discrete_x, solution_r[i, :], label = "Re(u), t = $(discrete_t[i])")
+    plot!(discrete_x, solution_r[i, :], label = "Re(u), t = $(discrete_t[i])",title = "Solution of Schrödingers equation", xaxis = "x (space)", yaxis = "u (Wave function)")
 end
 
 # Plot the imaginary part of the solution
 for i in eachindex(discrete_t)
-    plot!(discrete_x, solution_i[i, :], label = "Im(u), t = $(discrete_t[i])")
+    plot!(discrete_x, solution_i[i, :], label = "Im(u), t = $(discrete_t[i])",title = "Solution of Schrödingers equation", xaxis = "x (space)", yaxis = "u (Wave function)")
 end
 
 plt
 
 plt2 = plot()
-@gif for i in eachindex(discrete_t)
-    plt2 = plot(discrete_x, solution_r[i, :], label = "Re(u), t = $(discrete_t[i])", lw=2, ylim=(-1.0, 1.0), xlabel="x", ylabel="u")
+
+anim = @animate for i in eachindex(discrete_t)
+    plt2 = plot(discrete_x, solution_r[i, :], label = "Re(u), t = $(discrete_t[i])", 
+                lw=2, ylim=(-1.0, 1.0), xlabel="x", ylabel="u")
     plot!(discrete_x, solution_i[i, :], label = "Im(u), t = $(discrete_t[i])", lw=2)
-end every 1
+end
+
+# Save the animation as a GIF
+gif(anim, "wavefunction_evolution.gif", fps = 10)
+# Plot the real part of the solution
+plt3 = plot()
+
+plot!(discrete_x, solution_r[1, :], label = "Re(u), t = $(discrete_t[1])",title = "Solution of Schrödingers equation", xaxis = "x (space)", yaxis = "u (Wave function)")
+plot!(discrete_x, solution_i[1, :], label = "Im(u), t = $(discrete_t[1])",title = "Solution of Schrödingers equation", xaxis = "x (space)", yaxis = "u (Wave function)")
+plt3
+
+plt4 = plot()
+
+plot!(discrete_x, solution_r[end, :], label = "Re(u), t = $(discrete_t[end])",title = "Solution of Schrödingers equation", xaxis = "x (space)", yaxis = "u (Wave function)")
+plot!(discrete_x, solution_i[end, :], label = "Im(u), t = $(discrete_t[end])",title = "Solution of Schrödingers equation", xaxis = "x (space)", yaxis = "u (Wave function)")
+plt4
